8a6bf295b914428172d225d1b2ce2d278c4eba31
kquote03 <kquote03@gmail.com>
2022-11-27 14:48:50 +0400

    Change students page to marks page and use caption instead

--- markspage.php
+++ markspage.php
@@ -1,1 +1,35 @@
-<!--To be filled in later with table-->
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <Title>Marks Page</title>
+    <style>
+    table,td,th{ border: 4px solid black;
+    border-collapse:collapse; 
+    }
+    th{ padding: 15px;
+     background-color:#be8fdd;
+    }
+    td{ padding:"5px";
+    text-align:center;
+    }
+
+    </style>
+</head>
+<body>
+    <caption>Marks Page</caption>
+    <table style="width:80%" id="table">
+        <tr>
+            <th>Student ID </th>
+            <th>Course Nmae </th>
+            <th>Quiz 1 </th>
+            <th>Quiz 2 </th>   
+            <th>Assignment1 </th>
+            <th>Assignment2</th>
+            <th>Midterm </th>
+            <th>Final Exam </th>
+            <th>Frequency</th>
+        </tr>
+    </table>
+</body>
+</html> 
